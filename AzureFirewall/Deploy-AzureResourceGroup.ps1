0342ff891baa2fbf8e91fcc3357aca531aaae1e6
Len Volk <lvolkov@outlook.com>
2019-02-06 08:47:18 -0500

    renew the code

--- AzureFirewall/Deploy-AzureResourceGroup.ps1
+++ AzureFirewall/Deploy-AzureResourceGroup.ps1
@@ -6,8 +6,8 @@ Param(
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
-    [string] $TemplateFile = 'azuredeploy.json',
-    [string] $TemplateParametersFile = 'azuredeploy.parameters.json',
+    [string] $TemplateFile = 'AzureFirewall.json',
+    [string] $TemplateParametersFile = 'AzureFirewall.parameters.json',
     [string] $ArtifactStagingDirectory = '.',
     [string] $DSCSourceFolder = 'DSC',
     [switch] $ValidateOnly
